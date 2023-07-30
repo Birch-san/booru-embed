7ce6dc6f50ef1169070c1b0a551bf41087456543
Alex Birch <Birch-san@users.noreply.github.com>
2023-07-30 20:35:45 +0100

    start on tokenizer

--- script/tsv_tokenizer.py
+++ script/tsv_tokenizer.py
@@ -1,4 +1,57 @@
 import fileinput
+from dataclasses import dataclass, field
+from typing import List, Set, Literal, Dict
 
-for line in fileinput.input():
-  print(f'py: {line}')
+@dataclass
+class Vocab:
+  token_to_ix: Dict[str, int] = field(default_factory=lambda:{})
+  tokens: List[str] = field(default_factory=lambda:[])
+  def add_token(self, token: str) -> int:
+    if token in self.token_to_ix:
+      return self.token_to_ix[token]
+    token_ix: int = len(self.tokens)
+    self.tokens.append(token)
+    self.token_to_ix[token] = token_ix
+
+vocab = Vocab()
+
+for special_token in [
+  'BOS',
+  'EOS',
+  'PAD',
+  'MASK',
+  'COPYRIGHT_START',
+  'CHARACTER_START',
+  'ARTIST_START',
+  'META_START',
+  'GENERAL_START',
+  ',',
+]:
+  vocab.add_token(special_token)
+
+for rating in ['g', 'e', 's', 'q']:
+  vocab.add_token(f'rating:{rating}')
+
+for category, min_prevalence in zip(
+  ['artist', 'character', 'copyright', 'general', 'meta'],
+  [100, 128, 100, 128, 100],
+):
+  with fileinput.input(files=(f'out_prevalence_category/{category}.txt'), encoding='utf-8') as f:
+    for line in f:
+      stripped: str = line.rstrip('\n')
+      prevalence, token = stripped.split(' ', maxsplit=1)
+      prevalence = int(prevalence)
+      if prevalence <= min_prevalence:
+        break
+      vocab.add_token(token)
+
+with fileinput.input(files=('/Users/birch/machine-learning/danbooru-bigquery/danbooru-captions.tsv'), encoding='utf-8') as f:
+  # skip header line
+  next(f)
+  for line in f:
+    stripped: str = line.rstrip('\n')
+    tab_split: List[str] = stripped.split('\t')
+    id, rating, score, meta, general, artist, copyright, character = tab_split
+    id = int(id)
+    rating: Literal['g', 'e', 's', 'q'] = rating
+    pass
---
+++ shell/split-word-prevalence-category.sh
@@ -0,0 +1,88 @@
+#!/usr/bin/env bash
+DIRNAME="$(dirname "$0")"
+OUT_DIR="$DIRNAME/../out_prevalence_category"
+
+mkdir -p "$OUT_DIR"
+
+awk -F'\t' 'NR > 1 {
+  split($4, meta, " ");
+  for (label_key in meta)
+    counts[meta[label_key]]++;
+}
+END {
+  for (key in counts) {
+    print counts[key], key;
+  }
+}' <(csv2tsv -H ~/machine-learning/danbooru-bigquery/*.csv) | sort -rn > "$OUT_DIR/meta.txt"
+
+awk -F'\t' 'NR > 1 {
+  split($6, artist, " ");
+  for (label_key in artist)
+    counts[artist[label_key]]++;
+}
+END {
+  for (key in counts) {
+    print counts[key], key;
+  }
+}' <(csv2tsv -H ~/machine-learning/danbooru-bigquery/*.csv) | sort -rn > "$OUT_DIR/artist.txt"
+
+awk -F'\t' 'NR > 1 {
+  split($7, copyright, " ");
+  for (label_key in copyright)
+    counts[copyright[label_key]]++;
+}
+END {
+  for (key in counts) {
+    print counts[key], key;
+  }
+}' <(csv2tsv -H ~/machine-learning/danbooru-bigquery/*.csv) | sort -rn > "$OUT_DIR/copyright.txt"
+
+awk -F'\t' 'NR > 1 {
+  split($8, character, " ");
+  for (label_key in character)
+    counts[character[label_key]]++;
+}
+END {
+  for (key in counts) {
+    print counts[key], key;
+  }
+}' <(csv2tsv -H ~/machine-learning/danbooru-bigquery/*.csv) | sort -rn > "$OUT_DIR/character.txt"
+
+awk -F'\t' 'NR > 1 {
+  split($5, general, " ");
+  for (label_key in general) {
+    label = general[label_key];
+    if (length(label) <= 4) {
+      counts[label]++;
+    } else {
+      rest = label;
+      continue_label_loop = 0;
+      while (has_qualifier = match(rest, /^(.*)_\(([^)]*?)\)$/, qualifier_match)) {
+        rest = qualifier_match[1];
+        qualifier = qualifier_match[2];
+        counts[qualifier]++;
+        if (qualifier == "cosplay") {
+          # _(cosplay) qualifies a name label, so what precedes it can be used
+          # without any splitting
+          counts[rest]++;
+          continue_label_loop = 1;
+          break;
+        }
+      }
+      if (continue_label_loop)
+        continue;
+      split(rest, parts, /[-_]/);
+      for(i in parts) {
+        part = parts[i];
+        if (part) {
+          counts[part]++;
+        }
+      }
+    }
+  }
+}
+END {
+  for (key in counts) {
+    print counts[key], key;
+  }
+}' <(csv2tsv -H ~/machine-learning/danbooru-bigquery/*.csv) | sort -rn > "$OUT_DIR/general.txt"
