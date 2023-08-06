f4aa5e5c88a9b1602584e0965b312e515d4b054a
Alex Birch <Birch-san@users.noreply.github.com>
2023-08-06 20:01:40 +0100

    saving ragged array

--- script/tsv_bucketer.py
+++ script/tsv_bucketer.py
@@ -1,14 +1,11 @@
-import fileinput
 from typing import List, Optional
 import torch
 from torch import IntTensor, bucketize, tensor
-from contextlib import ExitStack
 from os import makedirs
 from os.path import join
 from tqdm import tqdm
-import pyarrow as pa
-from pyarrow.parquet import ParquetWriter
 import numpy as np
+from numpy.typing import NDArray
 
 from src.vocab import Vocab
 from src.tokenizer import make_tsv_record_to_token_ids, TsvRecordToTokenIds
@@ -17,10 +14,6 @@ vocab = Vocab()
 # create this file by running scripts/make_tokenizer.py
 with open('out_tokenizer/vocab.txt', mode='r', encoding='utf-8') as vocab_in:
   vocab.load(vocab_in)
-
-schema = pa.schema([
-  ('input_ids', pa.int16()),
-])
 
 # check we're safe to save the dataset in int16
 assert len(vocab.tokens) < (1 << 15)
@@ -34,46 +27,66 @@ assert len(vocab.tokens) < (1 << 15)
 #   t = torch.tensor([17, 36, 55, 73, 92, 111, 129, 148, 167, 185, 204, 223, 241], dtype=torch.int32)
 #   t[:-1] + (t.diff()/2).int()
 # let's round the final up to 255
-buckets: IntTensor = tensor([26,  45,  64,  82, 101, 120, 138, 157, 176, 194, 213, 232, 255], dtype=torch.int32)
+buckets: IntTensor = tensor([26,  45,  64,  82, 101, 120, 138, 157, 176, 194, 213, 232, 255], dtype=torch.int16)
 max_tokens: int = buckets.max().item()
 
 tsv_record_to_token_ids: TsvRecordToTokenIds = make_tsv_record_to_token_ids(vocab, do_shuffle=True, max_tokens=max_tokens, statistics=None)
 
-# out_dir = '/home/birch/ml-data/booru-captions-out-lenbucket'
 out_dir = 'out_lenbucket'
 makedirs(out_dir, exist_ok=True)
-bucket_filenames: List[str] = [f'bucket_{bucket}.parquet' for bucket in buckets]
-bucket_paths: List[str] = [join(out_dir, fname) for fname in bucket_filenames]
-# bucket_dirnames: List[str] = [f'bucket_{bucket}' for bucket in buckets]
-# bucket_dirs: List[str] = [join(out_dir, name) for name in bucket_dirnames]
-# print(f"making bucket dirs under {out_dir}: {bucket_dirnames}")
-# for bucket_dir in bucket_dirs:
-#   makedirs(bucket_dir, exist_ok=True)
 
-with ExitStack() as stack:
-  parquet_writers: List[ParquetWriter] = [
-    ParquetWriter(bucket_path, schema=schema) for bucket_path in bucket_paths
-  ]
-  for mgr in parquet_writers:
-    stack.enter_context(mgr)
+value_buckets: List[List[NDArray]] = [[] for _ in buckets]
+# index_buckets: List[List[int]] = [[0] for _ in buckets]
+length_buckets: List[List[int]] = [[] for _ in buckets]
 
-  with fileinput.input(files=('/Users/birch/machine-learning/danbooru-bigquery/danbooru-captions.tsv'), encoding='utf-8') as f:
-    # skip header line
-    next(f)
-    samples_estimate = 5770089
-    for line in tqdm(f, total=samples_estimate, unit=f'caption'):
-      stripped: str = line.rstrip('\n')
-      token_ids: Optional[List[int]] = tsv_record_to_token_ids(stripped)
-      if token_ids is None:
-        continue
-      # print([vocab.tokens[token_id] for token_id in token_ids])
-      
-      token_len: int = len(token_ids)
-      bucket_ix: int = bucketize(token_len, buckets).item()
+# this is just for testing
+target_count = 100
 
-      sink: ParquetWriter = parquet_writers[bucket_ix]
-      arr = pa.array(np.array(token_ids, np.int16), pa.int16(), mask=None, size=token_len)
-      table = pa.Table.from_arrays([arr], schema=schema)
-      sink.write_table(table)
-      pass
-  pass
+with open('/Users/birch/machine-learning/danbooru-bigquery/danbooru-captions.tsv', mode='r', encoding='utf-8') as f:
+  # skip header line
+  next(f)
+  samples_estimate = 5770089
+  for line in tqdm(f, total=samples_estimate, unit=f'caption'):
+    stripped: str = line.rstrip('\n')
+    token_ids: Optional[List[int]] = tsv_record_to_token_ids(stripped)
+    if token_ids is None:
+      continue
+    # print([vocab.tokens[token_id] for token_id in token_ids])
+    
+    token_len: int = len(token_ids)
+    bucket_ix: int = bucketize(token_len, buckets, out_int32=True).item()
+
+    value_bucket: List[NDArray] = value_buckets[bucket_ix]
+    # index_bucket: List[int] = index_buckets[bucket_ix]
+    length_bucket: List[int] = length_buckets[bucket_ix]
+
+    token_ids_n: NDArray = np.array(token_ids, np.int16)
+    value_bucket.append(token_ids_n)
+    # index_bucket.append(index_bucket[-1] + token_len)
+    length_bucket.append(token_len)
+
+    target_count -= 1
+    if target_count == 0:
+      break
+    pass
+
+# for bucket, value_bucket, index_bucket in zip(buckets, value_buckets, index_buckets):
+for bucket, value_bucket, length_bucket in zip(buckets, value_buckets, length_buckets):
+  if not value_bucket:
+    continue
+  # assert len(index_bucket)-1 == len(value_bucket)
+  assert len(length_bucket) == len(value_bucket)
+  values: NDArray = np.concatenate(value_bucket, axis=-1)
+  # indices: NDArray = np.array(index_bucket[:-1], dtype=np.int16)
+  lengths: NDArray = np.array(length_bucket, dtype=np.int16)
+
+  # you can compute indices from lengths. faster way to do this is to roll + assign a 0, but:
+  # indices: NDArray = np.pad(lengths, (1, 0)).cumsum()[:-1]
+
+  out_values: str = join(out_dir, f'b{bucket.item()}_values.npy')
+  # out_indices: str = join(out_dir, f'b{bucket.item()}_indices.npy')
+  out_lengths: str = join(out_dir, f'b{bucket.item()}_lengths.npy')
+  
+  np.save(out_values, values, allow_pickle=False)
+  # np.save(out_indices, indices, allow_pickle=False)
+  np.save(out_lengths, lengths, allow_pickle=False)
