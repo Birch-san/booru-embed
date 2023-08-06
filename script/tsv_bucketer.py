b22b3bf110594862a4b95cc7698768dd383bc8e6
Alex Birch <Birch-san@users.noreply.github.com>
2023-08-06 01:53:59 +0100

    dedupe tokenizer

--- script/tsv_bucketer.py
+++ script/tsv_bucketer.py
@@ -1,20 +1,17 @@
 import fileinput
-from typing import List, Dict, Any
+from typing import List, Optional
 import torch
 from torch import IntTensor, bucketize, tensor
 from contextlib import ExitStack
 from os import makedirs
 from os.path import join
 from tqdm import tqdm
-from random import shuffle
 import pyarrow as pa
 from pyarrow.parquet import ParquetWriter
 import numpy as np
 
 from src.vocab import Vocab
-from src.booru_special_tokens import make_rating_token, Rating, ratings, SpecialToken
-from src.general_label_to_ids import get_general_label_to_ids, GeneralLabelToIds
-from src.intersperse_flatten import intersperse_flatten
+from src.tokenizer import make_tsv_record_to_token_ids, TsvRecordToTokenIds
 
 vocab = Vocab()
 # create this file by running scripts/make_tokenizer.py
@@ -27,22 +24,6 @@ schema = pa.schema([
 
 # check we're safe to save the dataset in int16
 assert len(vocab.tokens) < (1 << 15)
-
-# micro-optimization to avoid dict lookup of tokens we'll be using often
-eos_token_id: int = vocab.token_to_ix[SpecialToken.EOS.value]
-char_token_id: int = vocab.token_to_ix[SpecialToken.CharacterStart.value]
-cpy_token_id: int = vocab.token_to_ix[SpecialToken.CopyrightStart.value]
-unk_token_id: int = vocab.token_to_ix[SpecialToken.Unknown.value]
-art_token_id: int = vocab.token_to_ix[SpecialToken.ArtistStart.value]
-meta_token_id: int = vocab.token_to_ix[SpecialToken.MetaStart.value]
-gen_token_id: int = vocab.token_to_ix[SpecialToken.GeneralStart.value]
-comma_token_id: int = vocab.token_to_ix[SpecialToken.Comma.value]
-cosp_token_id: int = vocab.token_to_ix[SpecialToken.Cosplay.value]
-
-# micro-optimization to, uh, look up from a smaller dict (might use linear probing rather than hash?)
-rating_token_ids: Dict[Rating, int] = {
-  rating: vocab.token_to_ix[make_rating_token(rating)] for rating in ratings
-}
 
 # determined via analysis of k-means (k=20) clusters over 5.7mill Danbooru captions.
 # *means* were:
@@ -57,6 +38,8 @@ rating_token_ids: Dict[Rating, int] = {
 buckets: IntTensor = tensor([22, 41, 60, 78, 97, 116, 134, 153, 172, 190, 209, 228, 245, 255], dtype=torch.int32)
 max_tokens: int = buckets.max().item()
 
+tsv_record_to_token_ids: TsvRecordToTokenIds = make_tsv_record_to_token_ids(vocab, do_shuffle=True, max_tokens=max_tokens, statistics=None)
+
 # out_dir = '/home/birch/ml-data/booru-captions-out-lenbucket'
 out_dir = 'out_lenbucket'
 makedirs(out_dir, exist_ok=True)
@@ -67,12 +50,6 @@ bucket_paths: List[str] = [join(out_dir, fname) for fname in bucket_filenames]
 # print(f"making bucket dirs under {out_dir}: {bucket_dirnames}")
 # for bucket_dir in bucket_dirs:
 #   makedirs(bucket_dir, exist_ok=True)
-
-def shuffle_(l: List[Any]) -> None:
-  if len(l) >= 2:
-    shuffle(l)
-
-general_label_to_ids: GeneralLabelToIds = get_general_label_to_ids(vocab)
 
 with ExitStack() as stack:
   parquet_writers: List[ParquetWriter] = [
@@ -87,57 +64,16 @@ with ExitStack() as stack:
     samples_estimate = 5770089
     for line in tqdm(f, total=samples_estimate, unit=f'caption'):
       stripped: str = line.rstrip('\n')
-      tab_split: List[str] = stripped.split('\t')
-      id, rating, score, meta, general, artist, copyright, character = tab_split
-      id = int(id)
-      rating: Rating = rating
-      # fallback to UNK for unknown character may be fine because we can correlate character with copyright and hair colour
-      char_token_ids: List[int] = [vocab.token_to_ix.get(tok, unk_token_id) for tok in character.split(' ') if tok in vocab.token_to_ix]
-      # fallback to UNK for copyright may be fine because we can correlate copyright with characters, or general labels
-      cpy_token_ids: List[int] = [vocab.token_to_ix.get(tok, unk_token_id) for tok in copyright.split(' ') if tok in vocab.token_to_ix]
-      # fallback to UNK for artist is a bit tenuous, but at least helps create an at-least-one-artist pattern, to help predict end-of-artist section
-      art_token_ids: List[int] = [vocab.token_to_ix.get(tok, unk_token_id) for tok in artist.split(' ') if tok in vocab.token_to_ix]
-      # probably not helpful to fallback to UNK for meta tokens, because they're not so correlated with other labels
-      meta_token_ids: List[int] = [vocab.token_to_ix.get(tok) for tok in meta.split(' ') if tok in vocab.token_to_ix]
-      general_labels: List[List[int]] = [general_label_to_ids(tok) for tok in general.split(' ')]
-
-      general_token_ids_len: int = sum((len(x) for x in general_labels)) + len(general_labels)-1
-      # compute length first, as a fast-path to discard long prompts before we commit to the cost of shuffling
-      token_len: int = 7 + len(char_token_ids) + len(cpy_token_ids) + len(art_token_ids) + general_token_ids_len + len(meta_token_ids)
-      if token_len > max_tokens:
-        # I mean we could drop labels to salvage it, but probably too many subjects are being portrayed to get a good embedding anyway
+      token_ids: Optional[List[int]] = tsv_record_to_token_ids(stripped)
+      if token_ids is None:
         continue
-
-      shuffle_(char_token_ids)
-      shuffle_(cpy_token_ids)
-      shuffle_(art_token_ids)
-      shuffle_(general_labels)
-      shuffle_(meta_token_ids)
-
-      general_token_ids: List[int] = list(intersperse_flatten(general_labels, comma_token_id))
-      assert len(general_token_ids) == general_token_ids_len
-
-      token_ixs: List[int] = [
-        rating_token_ids[rating],
-        char_token_id,
-        *char_token_ids,
-        cpy_token_id,
-        *cpy_token_ids,
-        art_token_id,
-        *art_token_ids,
-        gen_token_id,
-        *general_token_ids,
-        meta_token_id,
-        *meta_token_ids,
-        eos_token_id,
-      ]
-      # print([vocab.tokens[token_ix] for token_ix in token_ixs])
-      assert len(token_ixs) == token_len
+      # print([vocab.tokens[token_id] for token_id in token_ids])
       
+      token_len: int = len(token_ids)
       bucket_ix: int = bucketize(token_len, buckets).item()
 
       sink: ParquetWriter = parquet_writers[bucket_ix]
-      arr = pa.array(np.array(token_ixs, np.int16), pa.int16(), mask=None, size=token_len)
+      arr = pa.array(np.array(token_ids, np.int16), pa.int16(), mask=None, size=token_len)
       table = pa.Table.from_arrays([arr], schema=schema)
       sink.write_table(table)
       pass
---
+++ src/tokenizer.py
@@ -0,0 +1,101 @@
+from dataclasses import dataclass, field
+from typing import List, Protocol, Dict, Any, Optional
+from random import shuffle
+
+from .vocab import Vocab
+from .booru_special_tokens import Rating, SpecialToken, make_rating_token, ratings
+from .general_label_to_ids import GeneralLabelToIds, get_general_label_to_ids
+from .intersperse_flatten import intersperse_flatten
+
+class TsvRecordToTokenIds(Protocol):
+  def __call__(self, tabdelim: str) -> List[int]: ...
+
+def shuffle_(l: List[Any]) -> None:
+  if len(l) >= 2:
+    shuffle(l)
+
+@dataclass
+class Statistics:
+  caption_lengths: List[int] = field(default_factory=lambda: [])
+  general_label_lengths: List[int] = field(default_factory=lambda: [0]*100)
+
+def make_tsv_record_to_token_ids(
+  vocab: Vocab,
+  do_shuffle: bool,
+  max_tokens: int,
+  statistics: Optional[Statistics] = None,
+) -> TsvRecordToTokenIds:
+  # micro-optimization to avoid dict lookup of tokens we'll be using often
+  eos_token_id: int = vocab.token_to_ix[SpecialToken.EOS.value]
+  char_token_id: int = vocab.token_to_ix[SpecialToken.CharacterStart.value]
+  cpy_token_id: int = vocab.token_to_ix[SpecialToken.CopyrightStart.value]
+  unk_token_id: int = vocab.token_to_ix[SpecialToken.Unknown.value]
+  art_token_id: int = vocab.token_to_ix[SpecialToken.ArtistStart.value]
+  meta_token_id: int = vocab.token_to_ix[SpecialToken.MetaStart.value]
+  gen_token_id: int = vocab.token_to_ix[SpecialToken.GeneralStart.value]
+  comma_token_id: int = vocab.token_to_ix[SpecialToken.Comma.value]
+
+  # micro-optimization to, uh, look up from a smaller dict (might use linear probing rather than hash?)
+  rating_token_ids: Dict[Rating, int] = {
+    rating: vocab.token_to_ix[make_rating_token(rating)] for rating in ratings
+  }
+  
+  general_label_to_ids: GeneralLabelToIds = get_general_label_to_ids(vocab)
+
+  def tsv_record_to_token_ids(tabdelim: str) -> Optional[List[int]]:
+    tab_split: List[str] = tabdelim.split('\t')
+    id, rating, score, meta, general, artist, copyright, character = tab_split
+    id = int(id)
+    rating: Rating = rating
+    # fallback to UNK for unknown character may be fine because we can correlate character with copyright and hair colour
+    char_token_ids: List[int] = [vocab.token_to_ix.get(tok, unk_token_id) for tok in character.split(' ') if tok in vocab.token_to_ix]
+    # fallback to UNK for copyright may be fine because we can correlate copyright with characters, or general labels
+    cpy_token_ids: List[int] = [vocab.token_to_ix.get(tok, unk_token_id) for tok in copyright.split(' ') if tok in vocab.token_to_ix]
+    # fallback to UNK for artist is a bit tenuous, but at least helps create an at-least-one-artist pattern, to help predict end-of-artist section
+    art_token_ids: List[int] = [vocab.token_to_ix.get(tok, unk_token_id) for tok in artist.split(' ') if tok in vocab.token_to_ix]
+    # probably not helpful to fallback to UNK for meta tokens, because they're not so correlated with other labels
+    meta_token_ids: List[int] = [vocab.token_to_ix.get(tok) for tok in meta.split(' ') if tok in vocab.token_to_ix]
+    general_labels: List[List[int]] = [general_label_to_ids(tok) for tok in general.split(' ')]
+
+    general_token_ids_len: int = sum((len(x) for x in general_labels)) + len(general_labels)-1
+    # compute length first, as a fast-path to discard long prompts before we commit to the cost of shuffling
+    token_len: int = 7 + len(char_token_ids) + len(cpy_token_ids) + len(art_token_ids) + general_token_ids_len + len(meta_token_ids)
+    if token_len > max_tokens:
+      # I mean we could drop labels to salvage it, but probably too many subjects are being portrayed to get a good embedding anyway
+      return None
+
+    if do_shuffle:
+      shuffle_(char_token_ids)
+      shuffle_(cpy_token_ids)
+      shuffle_(art_token_ids)
+      shuffle_(general_labels)
+      shuffle_(meta_token_ids)
+
+    general_token_ids: List[int] = list(intersperse_flatten(general_labels, comma_token_id))
+    assert len(general_token_ids) == general_token_ids_len
+
+    token_ixs: List[int] = [
+      rating_token_ids[rating],
+      char_token_id,
+      *char_token_ids,
+      cpy_token_id,
+      *cpy_token_ids,
+      art_token_id,
+      *art_token_ids,
+      gen_token_id,
+      *general_token_ids,
+      meta_token_id,
+      *meta_token_ids,
+      eos_token_id,
+    ]
+    # print([vocab.tokens[token_ix] for token_ix in token_ixs])
+    assert len(token_ixs) == token_len
+
+    if statistics is not None:
+      for general_label in general_labels:
+        statistics.general_label_lengths[len(general_label)] += 1
+      statistics.caption_lengths.append(len(token_ixs))
+    
+    return token_ixs
+  
+  return tsv_record_to_token_ids
