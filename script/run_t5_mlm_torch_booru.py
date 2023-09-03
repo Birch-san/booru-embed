decoder attention mask + fixes for dataset loading

script/run_t5_mlm_torch_booru.py
@@ -175,14 +175,6 @@ class DataTrainingArguments:
     dataset_config_name: Optional[str] = field(
         default=None, metadata={"help": "The configuration name of the dataset to use (via the datasets library)."}
     )
-    train_file: Optional[str] = field(default=None, metadata={"help": "The input training data file (a text file)."})
-    validation_file: Optional[str] = field(
-        default=None,
-        metadata={"help": "An optional input evaluation data file to evaluate the perplexity on (a text file)."},
-    )
-    overwrite_cache: bool = field(
-        default=False, metadata={"help": "Overwrite the cached training and evaluation sets"}
-    )
     validation_split_percentage: Optional[int] = field(
         default=5,
         metadata={
@@ -240,23 +232,6 @@ class DataTrainingArguments:
             )
         },
     )
-    streaming: bool = field(default=False, metadata={"help": "Enable streaming mode"})
-
-    def __post_init__(self):
-        if self.streaming:
-            require_version("datasets>=2.0.0", "The streaming feature requires `datasets>=2.0.0`")
-
-        if self.dataset_name is None and self.train_file is None and self.validation_file is None:
-            raise ValueError("Need either a dataset name or a training/validation file.")
-        else:
-            if self.train_file is not None:
-                extension = self.train_file.split(".")[-1]
-                if extension not in ["csv", "json", "txt"]:
-                    raise ValueError("`train_file` should be a csv, a json or a txt file.")
-            if self.validation_file is not None:
-                extension = self.validation_file.split(".")[-1]
-                if extension not in ["csv", "json", "txt"]:
-                    raise ValueError("`validation_file` should be a csv, a json or a txt file.")
 
 
 def main():
@@ -425,19 +400,21 @@ def main():
     bucket_samples_test: Dict[int, BucketContent] = {}
 
     def get_indices(lengths: NDArray) -> NDArray:
-        indices: LongTensor = torch.from_numpy(lengths).to(device=device)
+        indices: LongTensor = torch.from_numpy(lengths).to(device=device, dtype=torch.long)
         indices.cumsum_(0)
         indices.resize_(indices.shape[0]+1)
         indices = indices.roll(1)
         return indices.cpu().numpy()
 
-    train_test_split = 0.1
+    train_test_split = data_args.validation_split_percentage/100
     # for testing
-    sample_limit_per_bucket = 100
+    sample_limit_per_bucket: Optional[int] = None if data_args.max_train_samples is None else (
+        int(data_args.max_train_samples/(1-train_test_split))
+    )
     for bucket_value, bucket_dir in zip(bucket_values, bucket_dirs):
         values: NDArray = np.load(join(bucket_dir, 'values.npy'))
         lengths: NDArray = np.load(join(bucket_dir, 'lengths.npy'))
-        samples_to_take = min(values.shape[0], sample_limit_per_bucket)
+        samples_to_take = lengths.shape[0] if sample_limit_per_bucket is None else min(lengths.shape[0], sample_limit_per_bucket)
 
         lengths = lengths[:samples_to_take]
         train_start_ix = int(samples_to_take * train_test_split)

src/model/modeling_t5_booru.py
@@ -22,7 +22,7 @@ import warnings
 from typing import Optional, Tuple, Union
 
 import torch
-from torch import nn, FloatTensor, LongTensor
+from torch import nn, FloatTensor, LongTensor, BoolTensor
 from torch.nn import LogSoftmax
 from torch.nn.functional import one_hot
 from torch.utils.checkpoint import checkpoint
@@ -866,9 +866,7 @@ class T5PreTrainedModel(PreTrainedModel):
             shifted_input_ids = torch.full(input_ids.shape[:-1] + (1,), decoder_start_token_id)
             shifted_input_ids = torch.cat([shifted_input_ids, input_ids[..., :-1]], dim=-1)
         else:
-            # TODO: should this just be detach().roll(input_ids, 1)?
-            shifted_input_ids = input_ids.new_zeros(input_ids.shape)
-            shifted_input_ids[..., 1:] = input_ids[..., :-1].clone()
+            shifted_input_ids = input_ids.detach().roll(1)
             shifted_input_ids[..., 0] = decoder_start_token_id
 
         if pad_token_id is None:
@@ -1714,8 +1712,11 @@ class T5BooruForMaskedLM(T5PreTrainedModel):
 
         if labels is not None and decoder_input_ids is None and decoder_inputs_embeds is None:
             # get decoder inputs from shifting lm labels to the right
-            # TODO: do we need to shift a mask too?
-            decoder_input_ids: LongTensor = self._shift_right(labels)
+            decoder_input_ids: LongTensor = self._shift_right(labels.int())
+
+        shifted_decoder_attention_mask: Optional[BoolTensor] = None
+        if decoder_attention_mask is not None:
+            shifted_decoder_attention_mask = self._shift_right(decoder_attention_mask)
 
         # Set device for model parallelism
         if self.model_parallel:
@@ -1725,13 +1726,13 @@ class T5BooruForMaskedLM(T5PreTrainedModel):
                 decoder_input_ids = decoder_input_ids.to(self.decoder.first_device)
             if attention_mask is not None:
                 attention_mask = attention_mask.to(self.decoder.first_device)
-            if decoder_attention_mask is not None:
-                decoder_attention_mask = decoder_attention_mask.to(self.decoder.first_device)
+            if shifted_decoder_attention_mask is not None:
+                shifted_decoder_attention_mask = shifted_decoder_attention_mask.to(self.decoder.first_device)
 
         # Decode
         decoder_outputs = self.decoder(
             input_ids=decoder_input_ids,
-            attention_mask=decoder_attention_mask,
+            attention_mask=shifted_decoder_attention_mask,
             inputs_embeds=decoder_inputs_embeds,
             past_key_values=past_key_values,
             encoder_hidden_states=hidden_states,
@@ -1780,9 +1781,10 @@ class T5BooruForMaskedLM(T5PreTrainedModel):
                 log_softmax: FloatTensor = lm_logits - log_z.unsqueeze(-1)
             else:
                 log_softmax: FloatTensor = self.loss_fn(lm_logits)
-            loss: FloatTensor = log_softmax.mul(labels_oh).sum(dim=-1).neg().mean()
+            loss: FloatTensor = log_softmax.mul(labels_oh).sum(dim=-1).neg()#.mean()
             if z_loss is not None and z_loss > 0.:
                 loss += z_loss * log_z ** 2
+            loss = loss.mean()
 
         if not return_dict:
             output = (lm_logits,) + decoder_outputs[1:] + encoder_outputs