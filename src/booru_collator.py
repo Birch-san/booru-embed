308083f1f1895244e832e9a5aa9167dd39cd8919
Alex Birch <Birch-san@users.noreply.github.com>
2023-09-09 16:28:07 +0100

    fix collator pad_to_multiple's unncessarily increasing length when you are already at that multiple. code-share between pad_to_max and pad_to_multiple

--- src/booru_collator.py
+++ src/booru_collator.py
@@ -52,6 +52,7 @@ class BooruDataCollatorForT5MLM:
         if self.pad_to_max:
             assert self.max_length is not None, 'You have requested pad_to_max, but you have not specified a max_length'
             assert self.max_length % self.pad_to_multiple == 0, f'You have requested pad_to_multiple={self.pad_to_multiple}, and pad_to_max=True. This requires your max_length={self.max_length} to be a multiple of pad_to_multiple={self.pad_to_multiple}.'
+            self.pad_to_multiple = self.max_length
 
     def __call__(self, examples: List[BooruDatum]) -> BatchEncoding:
         # tensorize input
@@ -83,12 +84,16 @@ class BooruDataCollatorForT5MLM:
 
         attention_mask: BoolTensor = batch_input_ids != self.pad_token_id
         decoder_attention_mask: BoolTensor = batch_labels != self.pad_token_id
+        
+        if self.max_length is not None:
+            assert batch_input_ids.shape[-1] <= self.max_length
+            assert attention_mask.shape[-1] <= self.max_length
+            assert batch_labels.shape[-1] <= self.max_length
+            assert decoder_attention_mask.shape[-1] <= self.max_length
 
-        if self.pad_to_multiple is not None or self.pad_to_max:
+        if self.pad_to_multiple is not None:
             input_length = batch_input_ids.shape[-1]
-            input_extra_tokens_needed: int = self.max_length - input_length if self.pad_to_max else (
-                self.pad_to_multiple - (input_length % self.pad_to_multiple)
-            )
+            input_extra_tokens_needed: int = (self.pad_to_multiple - (input_length % self.pad_to_multiple)) % self.pad_to_multiple
             # pad to multiple of (for example, 8) tokens
             batch_input_ids = pad(batch_input_ids, pad=(0, input_extra_tokens_needed), value=self.pad_token_id)
             attention_mask = pad(attention_mask, pad=(0, input_extra_tokens_needed))
@@ -96,17 +101,9 @@ class BooruDataCollatorForT5MLM:
             # TODO: do labels need padding too?
             # TODO: is it actually (input_ids + labels) we need to pad, rather than the sequences individually?
             label_length = batch_labels.shape[-1]
-            label_extra_tokens_needed: int = self.max_length - label_length if self.pad_to_max else (
-                self.pad_to_multiple - (label_length % self.pad_to_multiple)
-            )
+            label_extra_tokens_needed: int = (self.pad_to_multiple - (label_length % self.pad_to_multiple)) % self.pad_to_multiple
             batch_labels = pad(batch_labels, pad=(0, label_extra_tokens_needed), value=self.pad_token_id)
             decoder_attention_mask = pad(decoder_attention_mask, pad=(0, label_extra_tokens_needed))
-        
-        if self.max_length is not None:
-            assert batch_input_ids.shape[-1] <= self.max_length
-            assert attention_mask.shape[-1] <= self.max_length
-            assert batch_labels.shape[-1] <= self.max_length
-            assert decoder_attention_mask.shape[-1] <= self.max_length
 
         data = BooruBatchData(
             input_ids=batch_input_ids.detach().cpu(),
