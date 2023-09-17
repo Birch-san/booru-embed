fix sigma reparam (needs re-entrant gradient checkpointing + we should initialize with AMP context enabled)

src/model/modeling_t5_booru.py
@@ -312,10 +312,9 @@ class T5BooruDenseActDense(nn.Module):
         super().__init__()
         self.wi = nn.Linear(config.d_model, config.d_ff, bias=False)
         self.wo = nn.Linear(config.d_ff, config.d_model, bias=False)
-        # commented-out until we solve the mixed-precision problem on T5BooruDenseGatedActDense
-        # if config.use_sigma_reparam:
-        #     self.wi = SReparam(self.wi, **config.s_reparam_config)
-        #     self.wo = SReparam(self.wo, **config.s_reparam_config)
+        if config.use_sigma_reparam:
+            self.wi = SReparam(self.wi, **config.s_reparam_config)
+            self.wo = SReparam(self.wo, **config.s_reparam_config)
         self.dropout = nn.Dropout(config.dropout_rate)
         self.act = ACT2FN[config.dense_act_fn]
 
@@ -340,11 +339,10 @@ class T5BooruDenseGatedActDense(nn.Module):
         self.wi_0 = nn.Linear(config.d_model, config.d_ff, bias=False)
         self.wi_1 = nn.Linear(config.d_model, config.d_ff, bias=False)
         self.wo = nn.Linear(config.d_ff, config.d_model, bias=False)
-        # commented-out because there's a mixed-precision-related error on the backwards pass
-        # if config.use_sigma_reparam:
-        #     self.wi_0 = SReparam(self.wi_0, **config.s_reparam_config)
-        #     self.wi_1 = SReparam(self.wi_1, **config.s_reparam_config)
-        #     self.wo = SReparam(self.wo, **config.s_reparam_config)
+        if config.use_sigma_reparam:
+            self.wi_0 = SReparam(self.wi_0, **config.s_reparam_config)
+            self.wi_1 = SReparam(self.wi_1, **config.s_reparam_config)
+            self.wo = SReparam(self.wo, **config.s_reparam_config)
 
         self.dropout = nn.Dropout(config.dropout_rate)
         self.act = ACT2FN[config.dense_act_fn]
@@ -412,11 +410,11 @@ class T5BooruAttention(nn.Module):
         self.o = nn.Linear(self.inner_dim, self.d_model, bias=False)
 
         # commented-out because there's a mixed-precision-related error on the backwards pass
-        # if config.use_sigma_reparam:
-        #     self.q = SReparam(self.q, **config.s_reparam_config)
-        #     self.k = SReparam(self.k, **config.s_reparam_config)
-        #     self.v = SReparam(self.v, **config.s_reparam_config)
-        #     self.o = SReparam(self.o, **config.s_reparam_config)
+        if config.use_sigma_reparam:
+            self.q = SReparam(self.q, **config.s_reparam_config)
+            self.k = SReparam(self.k, **config.s_reparam_config)
+            self.v = SReparam(self.v, **config.s_reparam_config)
+            self.o = SReparam(self.o, **config.s_reparam_config)
 
         if self.has_relative_attention_bias:
             self.relative_attention_bias = nn.Embedding(self.relative_attention_num_buckets, self.n_heads)
@@ -1032,6 +1030,7 @@ class T5BooruStack(T5BooruPreTrainedModel):
     use_xformers_attn: bool
     embed_tokens: Optional[Embedding]
     conv_in: Optional[Conv1d]
+    needs_reentrant_checkpoint: bool
 
     def __init__(
         self,
@@ -1043,6 +1042,7 @@ class T5BooruStack(T5BooruPreTrainedModel):
 
         self.embed_tokens = embed_tokens
         self.is_decoder = config.is_decoder
+        self.needs_reentrant_checkpoint = config.use_sigma_reparam
 
         if config.use_conv_in:
             out_channels = config.d_model
@@ -1274,7 +1274,7 @@ class T5BooruStack(T5BooruPreTrainedModel):
                     layer_head_mask,
                     cross_attn_layer_head_mask,
                     None,  # past_key_value is always None with gradient checkpointing
-                    use_reentrant=False,
+                    use_reentrant=self.needs_reentrant_checkpoint,
                 )
             else:
                 layer_outputs = layer_module(

src/trainer_callbacks/sreparam_callback.py
@@ -4,14 +4,15 @@ from transformers import TrainerCallback, TrainingArguments, TrainerState, Train
 from ..model.modeling_t5_booru import T5BooruForMaskedLM
 from ..model.sigma_reparam import SReparam
 from ..booru_collator import BooruBatchData
-from typing import Optional
 from contextlib import nullcontext
 from logging import getLogger
+from dataclasses import dataclass, field
 
 logger = getLogger(__name__)
 
+@dataclass
 class SReparamCallback(TrainerCallback):
-  amp_context: Optional[torch.cuda.amp.autocast|nullcontext] = None
+  amp_context: torch.cuda.amp.autocast|nullcontext = field(default_factory=nullcontext)
 
   def on_train_begin(self, args: TrainingArguments, state: TrainerState, control: TrainerControl, **kwargs):
     model: T5BooruForMaskedLM = kwargs['model']
@@ -25,18 +26,10 @@ class SReparamCallback(TrainerCallback):
       decoder_input_ids=ones(*batch_shape, dtype=torch.int16, device=device),
       decoder_attention_mask=ones(*batch_shape, dtype=torch.bool, device=device),
     )
-    SReparam.init_all_(model, **batch)
+    with self.amp_context:
+      SReparam.init_all_(model, **batch)
   
   def on_step_end(self, args: TrainingArguments, state: TrainerState, control: TrainerControl, **kwargs):
     model: T5BooruForMaskedLM = kwargs['model']
-    if self.amp_context is None:
-      match args.half_precision_backend:
-        case 'auto' | 'cuda':
-          amp_dtype = torch.float16 if args.fp16 else torch.bfloat16
-          self.amp_context = torch.cuda.amp.autocast(cache_enabled=True, dtype=amp_dtype)
-        case _:
-          logger.warning(f'half_precision_backend={args.half_precision_backend} not implemented; disabling amp')
-          self.amp_context = nullcontext()
-
     with self.amp_context:
       SReparam.update_all_(model)