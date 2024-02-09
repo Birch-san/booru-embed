1c312cf983840cd437fe319bce54486fa1ad6fb5
Alex Birch <Birch-san@users.noreply.github.com>
2024-02-09 21:47:20 +0000

    fix booru_ver field

--- script/run_t5_mlm_torch_booru.py
+++ script/run_t5_mlm_torch_booru.py
@@ -194,7 +194,7 @@ class ModelArguments:
         },
     )
     actual_t5: bool = field(default=False, metadata={"help": 'original gangsta T5'})
-    booru_ver: str = field(default='v2', choices=['v1', 'v2'], metadata={"help": 'which version of the modeling_t5_booru code to use'})
+    booru_ver: str = field(default='v2', metadata={"help": 'which version of the modeling_t5_booru code to use', 'choices':['v1', 'v2']})
 
     def __post_init__(self):
         if self.config_overrides is not None and (self.config_name is not None or self.model_name_or_path is not None):
