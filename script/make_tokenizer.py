5098e5041d86a07d20dcc03ad2459b1190bd568b
Alex Birch <Birch-san@users.noreply.github.com>
2024-02-21 00:01:55 +0000

    retain the categories of each token

--- script/make_tokenizer.py
+++ script/make_tokenizer.py
@@ -2,15 +2,22 @@ from os import makedirs
 from os.path import join
 from src.vocab import Vocab
 from src.booru_special_tokens import get_booru_special_tokens
+from src.map_except_first import map_except_first
+from typing import Dict, Literal, List
 
 vocab = Vocab()
 
 for special in get_booru_special_tokens():
   vocab.add_token(special)
 
+Category = Literal['artist', 'character', 'copyright', 'general', 'meta']
+categories: List[Category] = ['artist', 'character', 'copyright', 'general', 'meta']
+tokens_by_category: Dict[Category, List[str]] = {
+  k: [] for k in categories
+}
 for category, min_prevalence in zip(
-  ['artist', 'character', 'copyright', 'general', 'meta'],
-  [100, 128, 100, 128, 100],
+  categories,
+  [128, 128, 100, 128, 100],
 ):
   # this file was created by:
   # - export CSV(s) via Google BigQuery (see README.md)
@@ -25,7 +32,9 @@ for category, min_prevalence in zip(
       prevalence = int(prevalence)
       if prevalence <= min_prevalence:
         break
-      vocab.add_token(token)
+      _, is_new = vocab.add_token(token)
+      if is_new:
+        tokens_by_category[category].append(token)
 
 # check we're safe to save the dataset in int16
 assert len(vocab.tokens) < (1 << 15)
@@ -35,3 +44,7 @@ makedirs(out_dir, exist_ok=True)
 out_file = join(out_dir, 'vocab.txt')
 with open(out_file, mode='w', encoding='utf-8') as vocab_out:
   vocab.save(vocab_out)
+for category in categories:
+  category_tokens: List[str] = tokens_by_category[category]
+  with open(join(out_dir, f'{category}.txt'), mode='w', encoding='utf-8') as category_out:
+    category_out.writelines(map_except_first(category_tokens, lambda token: '\n' + token))
--- src/vocab.py
+++ src/vocab.py
@@ -1,20 +1,25 @@
 from dataclasses import dataclass, field
-from typing import Dict, List
+from typing import Dict, List, NamedTuple
 from io import TextIOWrapper
 from .map_except_first import map_except_first
+
+class AddTokenResult(NamedTuple):
+  token_ix: int
+  is_new: bool
 
 @dataclass
 class Vocab:
   token_to_ix: Dict[str, int] = field(default_factory=lambda:{})
   tokens: List[str] = field(default_factory=lambda:[])
-  def add_token(self, token: str) -> int:
+  def add_token(self, token: str) -> AddTokenResult:
     if not token:
       raise ValueError('empty tokens are no bueno')
     if token in self.token_to_ix:
-      return self.token_to_ix[token]
+      return AddTokenResult(self.token_to_ix[token], False)
     token_ix: int = len(self.tokens)
     self.tokens.append(token)
     self.token_to_ix[token] = token_ix
+    return AddTokenResult(token_ix, True)
   
   def save(self, file: TextIOWrapper) -> None:
     """
